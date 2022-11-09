f4876160612d15481c3b220c5900e82f593d4b05
Pitra Pamungkas <ppamungkas@uplandsoftware.com>
2022-11-09 15:34:11 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { Ball } from './Ball';
+import { GameBox } from './GameBox';
+import { GameContainer } from './GameContainer';
+
+const BALL_SIZE = 20;
+const GAME_HEIGHT = 500;
+const GAME_WIDTH = 800;
 
 function App() {
+  const initialBallY = (GAME_HEIGHT - BALL_SIZE) / 2;
+  const initialBallX = (GAME_WIDTH + BALL_SIZE) / 2;
+  const [ballPositionX, setBallPositionX] = useState(initialBallX);
+  const [ballPositionY, setBallPositionY] = useState(initialBallY);
+  const [speedX, setSpeedX] = useState(0);
+  const [speedY, setSpeedY] = useState(1);
+
+  useEffect(() => {
+    let timerID;
+    if (timerID) clearInterval(timerID);
+    timerID = setInterval(
+      () => {
+        setBallPositionX(ballPositionX => ballPositionX + speedX);
+        setBallPositionY(ballPositionY => ballPositionY + speedY);
+      },
+      25
+    );
+
+    return () => clearInterval(timerID);
+  }, [speedX, speedY]);
+
+  useEffect(() => {
+    console.log(ballPositionX, ballPositionY, GAME_HEIGHT);
+    if (ballPositionY === GAME_HEIGHT - BALL_SIZE) {
+      setSpeedY(speed => -speed);
+    }
+    if (ballPositionY === 0) {
+      setSpeedY(speed => -speed);
+    }
+  }, [ballPositionX, ballPositionY]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GameContainer>
+      <GameBox width={GAME_WIDTH} height={GAME_HEIGHT}>
+        <Ball size={BALL_SIZE} ballPositionX={ballPositionX} ballPositionY={ballPositionY} />
+      </GameBox>
+    </GameContainer>
   );
 }
 
