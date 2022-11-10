3969e278c2bae26afbfa46f29fdd74a25f86cd96
Pitra Pamungkas <ppamungkas@uplandsoftware.com>
2022-11-10 09:23:18 +0700

    refactor game const

---
+++ src/GAME_CONST.js
@@ -0,0 +1,13 @@
+export const BALL_SIZE = 20;
+export const GAME_HEIGHT = 500;
+export const GAME_WIDTH = 800;
+export const INIT_SPEED = 5; //initial ball speed
+
+const INC_SPEED = 1; //increase speed on each touch by a factor of (not used yet)
+
+export const PADDLE_WIDTH = 10;
+export const PADDLE_HEIGHT = 100;
+export const PADDLE_SPEED = 35; //how fast paddle can move
+
+export const INTERVAL = 25; //used in setInterval
+
