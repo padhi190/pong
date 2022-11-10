4114357bfd3be3444be3380b1615841947c88264
Mark Hughes <mhughes@LHRPDHUGHESMB2.local>
2022-11-10 10:20:43 +0000

    tweaked game settings

--- src/GAME_CONST.js
+++ src/GAME_CONST.js
@@ -1,13 +1,13 @@
-export const BALL_SIZE = 20;
+export const BALL_SIZE = 15;
 export const GAME_HEIGHT = 500;
 export const GAME_WIDTH = 800;
-export const INIT_SPEED = 5; //initial ball speed
+export const INIT_SPEED = 6; //initial ball speed
 
-export const INC_SPEED = 1.1; //increase speed on each touch by a factor of (not used yet)
+export const INC_SPEED = 1.2; //increase speed on each touch by a factor of (not used yet)
 
-export const PADDLE_WIDTH = 10;
+export const PADDLE_WIDTH = 7;
 export const PADDLE_HEIGHT = 100;
-export const PADDLE_SPEED = 35; //how fast paddle can move
+export const PADDLE_SPEED = 100; //how fast paddle can move
 
-export const INTERVAL = 25; //used in setInterval
+export const INTERVAL = 12; //used in setInterval
 
